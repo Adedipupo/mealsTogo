052b753946177a553c1193eba9841d6277803b50
Benedict <jokanolaadedipupo@gmail.com>
2021-10-28 16:13:01 +0100

    theme

--- src/features/restuarants/screens/RestuarantScreen.js
+++ src/features/restuarants/screens/RestuarantScreen.js
@@ -1,26 +1,41 @@
 import React from "react";
 import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Searchbar } from 'react-native-paper';
+import styled from "styled-components";
 import { RestuarantInfoCard } from "../components/RestuarantInfoCard";
 
+const SafeArea = styled(SafeAreaView)`
+   flex: 1;
+   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const RestuarantInfoCardContainer = styled(View)`
+   flex: 1; 
+   padding: 16px;
+   backgroundColor: blue; 
+`;
+
+const SearchContainer = styled(View)`
+  padding: 16px;
+`;
 
 export const RestuarantScreen = () => {
     return (
-    <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-    <View style={styles.search}>
+    <SafeArea>
+    <SearchContainer>
       <Searchbar
         placeholder="e.g San Siro"
       />   
-    </View>
-    <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+    </SearchContainer>
+    <RestuarantInfoCardContainer>
       <RestuarantInfoCard />
-    </View>
-  </SafeAreaView>
+    </RestuarantInfoCardContainer>
+  </SafeArea>
   )
 }
 
 const styles = StyleSheet.create({
     search: {
-       padding: 16
+       
     }
   });
