16c62da426d5adcbabe9ccbfb321fb3fad71637c
Benedict <jokanolaadedipupo@gmail.com>
2021-10-29 17:56:04 +0100

    tab-bottom-nav

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
-import  {RestuarantScreen}  from './src/features/restuarants/screens/RestuarantScreen';
+import { RestuarantScreen } from './src/features/restuarants/screens/RestuarantScreen';
 import { theme } from './src/infrastructure/theme';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -12,6 +16,24 @@ import {
   Lato_400Regular,
 } from '@expo-google-fonts/lato';
 
+
+function MapScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -26,10 +48,37 @@ export default function App() {
   }
   return (
     <>
-     <ThemeProvider theme={theme}>
-      <RestuarantScreen />
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Tab.Navigator
+          screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            } else if (route.name === 'Map') {
+              iconName = focused ? 'ios-map' : 'ios-map';
+            }else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-settings' : 'ios-settings';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+          >
+            <Tab.Screen name="Home" component={RestuarantScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
       <StatusBar style="auto" />
-     </ThemeProvider>
     </>
   );
 }
